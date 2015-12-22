5461d8d36773d30fb37ae1fc58ec0ee7624f0d38
erdmko <erdmko@gmail.com>
2015-12-22 15:09:21 -0500

    Deploy

--- mesto/settings/base.py
+++ mesto/settings/base.py
@@ -110,6 +110,9 @@ DATABASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT  = os.path.join(BASE_DIR, '../static/')
+MEDIA_ROOT  = os.path.join(BASE_DIR, '../media/')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 STATIC_URL = '/static/'
 CKEDITOR_CONFIGS = {
